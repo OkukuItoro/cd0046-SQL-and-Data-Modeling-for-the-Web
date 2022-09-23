391328db20bcd2634efc875a9d83f21c4fbfaaac
Patrick Itorobong Okuku <patrickokuku1@gmail.com>
2022-09-23 03:53:46 +0100

    form validation update

--- forms.py
+++ forms.py
@@ -5,11 +5,7 @@ from wtforms.validators import DataRequired, AnyOf, URL
 import re
 from wtforms import ValidationError
 
-def validate_phone(phone):
-    us_phone_num = '^([0-9]{3})[-][0-9]{3}[-][0-9]{4}$'
-    match = re.search(us_phone_num, phone.data)
-    if not match:
-        raise ValidationError('Error, phone number must be in format xxx-xxx-xxxx')
+
 
 class ShowForm(Form):
     artist_id = StringField(
@@ -137,6 +133,12 @@ class VenueForm(Form):
 
 
 class ArtistForm(Form):
+    def validate_phone(self, phone):
+        us_phone_num = '^([0-9]{3})[-][0-9]{3}[-][0-9]{4}$'
+        match = re.search(us_phone_num, phone.data)
+        if not match:
+            raise ValidationError('Error, phone number must be in format xxx-xxx-xxxx')
+
     name = StringField(
         'name', validators=[DataRequired()]
     )
@@ -201,9 +203,8 @@ class ArtistForm(Form):
     )
     phone = StringField(
         # TODO implement validation logic for state
-        'phone'
+        'phone',
     )
-
     validate_phone(phone)
     
     image_link = StringField(
